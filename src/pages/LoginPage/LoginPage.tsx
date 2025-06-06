d56f9aace0a8ead45208d99e5eb640ebf3661985
Muskaan Gupta <Muskaan Gupta>
2025-06-06 12:24:26 +0530

    fix issue

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import { useHistory, useLocation } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 import useAuth from '../../auth/useAuth'
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface LoginPageProps {}
 
 const LoginPage: React.FC<LoginPageProps> = () => {
-  const history = useHistory()
+  const navigate = useNavigate()
   const location = useLocation()
   const auth = useAuth()
 
   const { from } = (location.state as any) || { from: { pathname: '/' } }
   const handleLogin = () => {
     auth?.signIn(() => {
-      history.replace(from)
+      navigate(from, {replace: true});
     })
   }
   return (
